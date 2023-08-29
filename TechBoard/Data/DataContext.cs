9cef700a6d2ca9364e88b59ce0fac9faa23af047
Daniel Berkowicz <daniel.berkowicz1@gmail.com>
2023-08-29 15:08:10 +0200

    Created DB codefirst

---
+++ TechBoard/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechBoard.Models;
+
+namespace TechBoard.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Subject> Subject { get; set; }
+        public DbSet<Models.Thread> Thread { get; set; }
+        public DbSet<Post> Post { get; set; }
+    }
+}
---
+++ TechBoard/Migrations/20230829130415_bump.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechBoard.Migrations
+{
+    /// <inheritdoc />
+    public partial class bump : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Subject",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subject", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Thread",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Heading = table.Column<int>(type: "int", nullable: false),
+                    SubjectRefId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Thread", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Thread_Subject_SubjectRefId",
+                        column: x => x.SubjectRefId,
+                        principalTable: "Subject",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TextBody = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ThreadtRefId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Post_Thread_ThreadtRefId",
+                        column: x => x.ThreadtRefId,
+                        principalTable: "Thread",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_ThreadtRefId",
+                table: "Post",
+                column: "ThreadtRefId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Thread_SubjectRefId",
+                table: "Thread",
+                column: "SubjectRefId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Post");
+
+            migrationBuilder.DropTable(
+                name: "Thread");
+
+            migrationBuilder.DropTable(
+                name: "Subject");
+        }
+    }
+}
