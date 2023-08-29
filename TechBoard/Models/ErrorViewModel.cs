10da2cd1d1fccdc77f822a41e28543e64b556799
Leo <115810719+L-stal@users.noreply.github.com>
2023-08-29 11:31:20 +0200

    start

---
+++ TechBoard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TechBoard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
