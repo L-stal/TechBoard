3010e7d54c6cd2c4d080d838b7bca1a95f53b0d0
Steven <steven.dalfall@live.se>
2023-08-30 14:38:42 +0200

    Working on Threads

---
+++ TechBoard/Controllers/ThreadController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechBoard.Models.ViewModels;
+
+namespace TechBoard.Controllers
+{
+    public class ThreadController : Controller
+    {
+        public IActionResult Index()
+        {
+            var thread0 = new ThreadViewModel { Id = 0, Heading = "Bästa GPU?", SubjectRefId = 0 };
+            var thread1 = new ThreadViewModel { Id = 1, Heading = "Photoshop version", SubjectRefId = 1 };
+            var thread2 = new ThreadViewModel { Id = 2, Heading = "Senaste uppdateringen av VSCode", SubjectRefId = 1 };
+
+            var threads = new List<ThreadViewModel> { thread0, thread1, thread2 };
+            
+            return View(threads);
+        }
+    }
+}
--- TechBoard/Models/ViewModels/ThreadViewModel.cs
+++ TechBoard/Models/ViewModels/ThreadViewModel.cs
@@ -1,6 +1,15 @@
-﻿namespace TechBoard.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechBoard.Models.ViewModels
 {
     public class ThreadViewModel
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Heading { get; set; }
+
+        public int SubjectRefId { get; set; }
     }
 }
