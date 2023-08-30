f474b8b2674289023ca13cc694f5bd1a1e7b4ee9
Wettergrund <Jonas.wettergrund@gmail.com>
2023-08-30 14:35:47 +0200

    Fixed roting for subjects

--- TechBoard/Controllers/SubjectController.cs
+++ TechBoard/Controllers/SubjectController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TechBoard.Models;
 
 namespace TechBoard.Controllers
 {
     public class SubjectController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string title)
         {
-            Console.WriteLine("Subject!");
-            return View();
+
+            title = char.ToUpper(title[0]) + title.Substring(1);
+
+            Subject subject = new Subject { Id = 1, Title = title };            
+            return View(subject);
         }
     }
 }
